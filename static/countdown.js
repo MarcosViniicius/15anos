f5278e63f3b98b4fc70434693c58d7cc19561cdc
Marcos Vinicius <marcosvinicius.lucena27@gmail.com>
2025-04-10 19:41:37 -0300

    melhorias no visual e adição de informações complementares.

--- static/countdown.js
+++ static/countdown.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-  const countDate = new Date("Jun 3, 2025 00:00:00").getTime();
+  const countDate = new Date("Jun 14, 2025 20:00:00").getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
